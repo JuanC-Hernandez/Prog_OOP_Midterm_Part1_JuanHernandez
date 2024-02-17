fa569af61345ac0b52567d14b906caa0dbd1186b
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-02-17 10:14:15 -0800

    1st commit

--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@
 {
     internal class Program
     {
+
+        // Juan Hernandez
+        // Midterm - Part 1
+        // 02/17/2024
         static void Main()
         {
+            List<string> localList = new List<string>();   
 
         }        
     }
